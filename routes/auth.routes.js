ac3d034145db003e6be434d67f92f282d9b15343
margri764 <62308899+margri764@users.noreply.github.com>
2022-02-25 18:16:05 -0300

    tercer deploy

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,9 +8,9 @@ const { checkFields } = require ('../middlewares');
 
 
 router.post('/login',[
-    check('email','el correo no es valido').isEmail(),
-    check('password','El password es obligatorio, mas de 6 letras').not().isEmpty(),
-    checkFields
+    // check('email','el correo no es valido').isEmail(),
+    // check('password','El password es obligatorio, mas de 6 letras').not().isEmpty(),
+    // checkFields
 ],signUp);  
 
 router.get('/confirm/:token',[
